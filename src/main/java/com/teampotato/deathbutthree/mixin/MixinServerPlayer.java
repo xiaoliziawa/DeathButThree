794345b81f2d6774aedc8ee4810b91af7bbffafa
Kasualix <670067575@qq.com>
2023-11-21 19:48:51 +0800

    use smoother way

--- src/main/java/com/teampotato/deathbutthree/mixin/MixinServerPlayer.java
+++ src/main/java/com/teampotato/deathbutthree/mixin/MixinServerPlayer.java
@@ -7,7 +7,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.player.ProfilePublicKey;
@@ -17,7 +16,6 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -32,28 +30,18 @@ public abstract class MixinServerPlayer extends Player {
         super(pLevel, pPos, pYRot, pGameProfile, pProfilePublicKey);
     }
 
-    @Unique
-    private static final RemovalReason deathButThree$discarded = RemovalReason.DISCARDED;
-
     @Inject(method = "die", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z", ordinal = 0, shift = At.Shift.BEFORE))
     private void onDie(DamageSource damageSource, CallbackInfo ci) {
         if (damageSource == null) return;
-        Entity sourceEntity = damageSource.getEntity();
-        if (sourceEntity instanceof LivingEntity sourceLiving) {
+        if (damageSource.getEntity() instanceof LivingEntity sourceLiving) {
             ResourceLocation sourceName = ForgeRegistries.ENTITY_TYPES.getKey(sourceLiving.getType());
             if (sourceName != null && Config.bosses.get().contains(sourceName.toString())) {
                 Set<String> tags = sourceLiving.getTags();
                 String uuid = sourceLiving.getStringUUID();
                 int maxDeathAmount = Config.maxDeathTimes.get();
                 if (tags.contains("death_but_" + (maxDeathAmount - 1) + uuid) || maxDeathAmount == 1) {
-                    if (maxDeathAmount != 1) {
-                        tags.remove("death_but_" + (maxDeathAmount - 1) + uuid);
-                    }
-                    MinecraftServer server = this.server;
-                    server.getCommands().performPrefixedCommand(server.createCommandSourceStack().withSuppressedOutput(),
-                            "execute in " + sourceLiving.level.dimension().location() + " run summon " + sourceName + " " +
-                                    sourceLiving.getX() + " " + sourceLiving.getY() + " " + sourceLiving.getZ());
-                    sourceLiving.remove(deathButThree$discarded);
+                    if (maxDeathAmount != 1) tags.remove("death_but_" + (maxDeathAmount - 1) + uuid);
+                    sourceLiving.heal(sourceLiving.getMaxHealth());
                 } else {
                     boolean have_died = false;
                     for (int deathAmount = maxDeathAmount - 2; deathAmount > 0; deathAmount--) {
